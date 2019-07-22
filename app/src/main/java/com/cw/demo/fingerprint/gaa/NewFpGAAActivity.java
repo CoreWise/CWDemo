91972b4745939ba864fa7c28f8f4971c50c64b2d
jyf <jin.yufan@corewise.cn>
2019-07-22 10:16:39 +0800

    add jyf by 20190706 msg:gaa更新

--- app/src/main/java/com/cw/demo/fingerprint/gaa/NewFpGAAActivity.java
+++ app/src/main/java/com/cw/demo/fingerprint/gaa/NewFpGAAActivity.java
@@ -22,9 +22,10 @@ import android.widget.Toast;
 
 import com.cw.demo.MyApplication;
 import com.cw.demo.R;
-import com.cw.fpgaasdk.FingerGAAFactory;
-import com.cw.fpgaasdk.GAANewAPI;
-import com.cw.fpgaasdk.GAAOldAPI;
+import com.cw.fpgaasdk.GaaApiBHMDevice;
+import com.cw.fpgaasdk.GaaApiBase;
+import com.cw.fpgaasdk.GaaApiZiDevice;
+import com.cw.fpgaasdk.GaaFingerFactory;
 import com.cw.fpgaasdk.USBFingerManager;
 import com.cw.serialportsdk.utils.DataUtils;
 import com.fm.bio.ID_Fpr;
@@ -46,7 +47,7 @@ public class NewFpGAAActivity extends Activity implements OnClickListener {
     boolean globalControl = true;
 
     //    public GAA_API mGaaApi;
-    public FingerGAAFactory mGaaApi;
+    public GaaApiBase mGaaApi;
 
 
     @SuppressLint("HandlerLeak")
@@ -124,33 +125,37 @@ public class NewFpGAAActivity extends Activity implements OnClickListener {
             @Override
             public void onOpenUSBFingerSuccess(String device) {
                 MyApplication.getApp().cancleProgressDialog();
-                if (device.equals(USBFingerManager.BYD_BIG_DEVICE2)) {
+                if (device.equals(GaaApiBase.ZiDevice)) {
+                    //新固件
+                    mGaaApi = new GaaFingerFactory().createGAAFinger(GaaApiBase.ZiDevice,NewFpGAAActivity.this);
 
-                    mGaaApi = new GAANewAPI(NewFpGAAActivity.this);
-//                    mGaaApi = new GAA_API(NewFpGAAActivity.this);
+//                    mGaaApi = new GAANewAPI(NewFpGAAActivity.this);
                     int ret = mGaaApi.openGAA();
-                    if (ret == FingerGAAFactory.LIVESCAN_SUCCESS) {
+                    if (ret == GaaApiBase.LIVESCAN_SUCCESS) {
                         updateMsg("open device success 1");
-                    } else if (ret == FingerGAAFactory.LIVESCAN_NOTINIT) {
+                    } else if (ret == GaaApiBase.LIVESCAN_NOTINIT) {
                         updateMsg("not init");
-                    } else if (ret == FingerGAAFactory.LIVESCAN_AUTH_FAILED) {
+                    } else if (ret == GaaApiBase.LIVESCAN_AUTH_FAILED) {
                         updateMsg("auth failed");
                     } else {
                         updateMsg("unknown " + ret);
                     }
 
-                } else if (device.equals(USBFingerManager.BYD_BIG_DEVICE)) {
-                    mGaaApi = new GAAOldAPI(NewFpGAAActivity.this);
+                } else if (device.equals(GaaApiBase.BHMDevice)) {
+                    //旧固件
+                    mGaaApi = new GaaFingerFactory().createGAAFinger(GaaApiBase.BHMDevice,NewFpGAAActivity.this);
 
+
+//                    mGaaApi = new GAAOldAPI(NewFpGAAActivity.this);
                     new Handler().postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            int ret = mGaaApi.openGAA();
-                            if (ret == FingerGAAFactory.LIVESCAN_SUCCESS) {
+                            int ret = ((GaaApiBHMDevice)mGaaApi).openGAA();
+                            if (ret == GaaApiBase.LIVESCAN_SUCCESS) {
                                 updateMsg("open device success 2");
-                            } else if (ret == FingerGAAFactory.LIVESCAN_NOTINIT) {
+                            } else if (ret == GaaApiBase.LIVESCAN_NOTINIT) {
                                 updateMsg("not init");
-                            } else if (ret == FingerGAAFactory.LIVESCAN_AUTH_FAILED) {
+                            } else if (ret == GaaApiBase.LIVESCAN_AUTH_FAILED) {
                                 updateMsg("auth failed");
                             } else {
                                 String msg = mGaaApi.ErrorInfo(ret);
@@ -227,7 +232,7 @@ public class NewFpGAAActivity extends Activity implements OnClickListener {
             case R.id.clear:
                 updateMsg(null);
 
-                if (FingerGAAFactory.LIVESCAN_SUCCESS != mGaaApi.PSEmpty()) {
+                if (GaaApiBase.LIVESCAN_SUCCESS != mGaaApi.PSEmpty()) {
                     updateMsg("清空指纹库失败");
                 }
                 updateMsg("清空指纹库成功");
@@ -249,13 +254,14 @@ public class NewFpGAAActivity extends Activity implements OnClickListener {
                 if (globalControl == false) {
                     return -1;
                 }
-                while (mGaaApi.PSGetImage() != FingerGAAFactory.LIVESCAN_NO_FINGER) {
+                while (mGaaApi.PSGetImage() != GaaApiBase.LIVESCAN_NO_FINGER) {
                     if (globalControl == false) {
                         return -1;
                     }
+                    publishProgress("mGaaApi.PSGetImage():" + mGaaApi.PSGetImage());
                     sleep(20);
                 }
-                while (mGaaApi.PSGetImage() == FingerGAAFactory.LIVESCAN_NO_FINGER) {
+                while (mGaaApi.PSGetImage() == GaaApiBase.LIVESCAN_NO_FINGER) {
 
                     if (globalControl == false) {
                         return -1;
@@ -304,21 +310,21 @@ public class NewFpGAAActivity extends Activity implements OnClickListener {
                 if (globalControl == false) {
                     return -1;
                 }
-                while (mGaaApi.PSGetImage() != FingerGAAFactory.LIVESCAN_NO_FINGER) {
+                while (mGaaApi.PSGetImage() != GaaApiBase.LIVESCAN_NO_FINGER) {
                     if (globalControl == false) {
                         return -1;
                     }
 
                     sleep(20);
                 }
-                while (mGaaApi.PSGetImage() == FingerGAAFactory.LIVESCAN_NO_FINGER) {
+                while (mGaaApi.PSGetImage() == GaaApiBase.LIVESCAN_NO_FINGER) {
                     if (globalControl == false) {
                         return -1;
                     }
                     sleep(10);
                 }
 
-                if ((ret = mGaaApi.PSUpImage(mGaaApi.PS_FingerBuf)) != FingerGAAFactory.LIVESCAN_SUCCESS) {
+                if ((ret = mGaaApi.PSUpImage(mGaaApi.PS_FingerBuf)) != GaaApiBase.LIVESCAN_SUCCESS) {
                     publishProgress("上传图像失败:" + ret);
                     continue;
                 }
@@ -327,7 +333,7 @@ public class NewFpGAAActivity extends Activity implements OnClickListener {
                 byte[] mFeature = new byte[ID_Fpr.LIVESCAN_FEATURE_SIZE];
                 int[] fingerId = new int[1];
                 //生成模板
-                if ((ret = mGaaApi.PSGenChar(mFeature, fingerId)) != FingerGAAFactory.LIVESCAN_SUCCESS) {
+                if ((ret = mGaaApi.PSGenChar(mFeature, fingerId)) != GaaApiBase.LIVESCAN_SUCCESS) {
                     publishProgress("生成特征失败:" + ret);
                     continue;
                 } else {
@@ -389,25 +395,25 @@ public class NewFpGAAActivity extends Activity implements OnClickListener {
                 if (globalControl == false) {
                     return -1;
                 }
-                while (mGaaApi.PSGetImage() == FingerGAAFactory.LIVESCAN_NO_FINGER) {
+                while (mGaaApi.PSGetImage() == GaaApiBase.LIVESCAN_NO_FINGER) {
                     if (globalControl == false) {
                         return -1;
                     }
                     sleep(20);
                 }
-                if ((ret = mGaaApi.PSUpImage(mGaaApi.PS_FingerBuf)) != FingerGAAFactory.LIVESCAN_SUCCESS) {
+                if ((ret = mGaaApi.PSUpImage(mGaaApi.PS_FingerBuf)) != GaaApiBase.LIVESCAN_SUCCESS) {
                     continue;
                 }
                 publishProgress("OK");
 
                 byte[] mFeature = new byte[ID_Fpr.LIVESCAN_FEATURE_SIZE];
-                if (mGaaApi.PSGenChar(mFeature) != FingerGAAFactory.LIVESCAN_SUCCESS) {
+                if (mGaaApi.PSGenChar(mFeature) != GaaApiBase.LIVESCAN_SUCCESS) {
                     continue;
                 }
                 time = System.currentTimeMillis();
 
                 int code = mGaaApi.PSSearch(mFeature, fingerId);
-                if (FingerGAAFactory.LIVESCAN_SUCCESS != code) {//mGaaApi.PSSearch(mFeature, fingerId)
+                if (GaaApiBase.LIVESCAN_SUCCESS != code) {//mGaaApi.PSSearch(mFeature, fingerId)
                     publishProgress("没有找到此指纹");
                     publishProgress("code = "+code);
                     continue;
@@ -437,92 +443,6 @@ public class NewFpGAAActivity extends Activity implements OnClickListener {
             updateMsg(values[0]);
         }
     }
-
-//    /**
-//     * 录入指纹
-//     */
-//    @SuppressLint("StaticFieldLeak")
-//    public class ImputAsyncTask2 extends AsyncTask<Integer, String, Integer> {
-//        int Progress = 0;
-//
-//        @Override
-//        protected Integer doInBackground(Integer... params) {
-//            int cnt = 1;
-//            int ret;
-//
-//            while (true) {
-//                if (globalControl == false) {
-//                    return -1;
-//                }
-//                while (mGaaApi.PSGetImage() != GAA_API.LIVESCAN_NO_FINGER) {
-//                    if (globalControl == false) {
-//                        return -1;
-//                    }
-//
-//                    sleep(20);
-//                }
-//                while (mGaaApi.PSGetImage() == GAA_API.LIVESCAN_NO_FINGER) {
-//                    if (globalControl == false) {
-//                        return -1;
-//                    }
-//                    sleep(10);
-//                }
-//
-//                if ((ret = mGaaApi.PSUpImage(mGaaApi.PS_FingerBuf)) != GAA_API.LIVESCAN_SUCCESS) {
-//                    publishProgress("上传图像失败:" + ret);
-//                    continue;
-//                }
-//                publishProgress("OK");
-//
-//                byte[] mFeature = new byte[ID_Fpr.LIVESCAN_FEATURE_SIZE];
-//                int[] fingerId = new int[1];
-//                //生成模板
-//                if ((ret = mGaaApi.PSGenChar2(mFeature,fingerId)) != GAA_API.LIVESCAN_SUCCESS) {
-//                    publishProgress("生成特征失败:" + ret);
-//                    continue;
-//                } else {
-//                    publishProgress("updateProgress");
-//                }
-//
-//                String s = DataUtils.bytesToHexString(mFeature);
-//                publishProgress(s);
-//                publishProgress("录入指纹成功,=====>ID:" + fingerId[0]);
-//                return 0;
-//            }
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Integer result) {
-//            globalControl = false;
-//            if (0 == result) {
-//                bar.setProgress(100);
-//                btnStatus(true);
-//            } else {
-//                bar.setProgress(0);
-//                updateMsg("指纹录入失败，请重新录入");
-//                globalControl = false;
-//            }
-//        }
-//
-//        @Override
-//        protected void onPreExecute() {
-//            updateMsg("录入指纹=>开始,请放上手指");
-//        }
-//
-//        @Override
-//        protected void onProgressUpdate(String... values) {
-//            if (values[0].equals("OK")) {
-//                updateFingerImg(mGaaApi.PS_FingerBuf);
-//                return;
-//            }
-//            if (values[0].equals("updateProgress")) {
-//                Progress += 50;
-//                bar.setProgress(Progress);
-//                return;
-//            }
-//            updateMsg(values[0]);
-//        }
-//    }
 
     private void updateFingerImg(byte[] fpBmp) {
         try {
